08c48062c94c06bd73226383adc2aa34bab84af4
Adrianne Magracia <adriannemagracia.git@gmail.com>
2024-10-23 20:29:27 +0800

    Refactor auto-save plugin

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -1,9 +1,3 @@
 return {
     "pocco81/auto-save.nvim",
-    config = function()
-        require("auto-save").setup({
-            enabled = true,
-            trigger_events = { "InsertLeave", "TextChanged" },
-        })
-    end,
 }
