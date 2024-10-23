0e21752152c14a2562c289b4c705d2724e6a4e6f
Adrianne Magracia <adriannemagracia.git@gmail.com>
2024-10-23 19:58:15 +0800

    Initial commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,17 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local function map(mode, lhs, rhs, opts)
+    local options = { noremap = true, silent = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.keymap.set(mode, lhs, rhs, options)
+end
+
+local function nmap(lhs, rhs, opts)
+    map("n", lhs, rhs, opts)
+end
+
+nmap("<C-a>", 'ggVG"+y', { desc = "Copy entire file content" })
