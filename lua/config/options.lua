0eb7409aba758bc966e344cf26eb9f90100d91e4
Adrianne Magracia <adriannemagracia.git@gmail.com>
2024-10-25 15:19:56 +0800

    Tweak options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,9 +1,20 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
 local opt = vim.opt
+
+vim.g.mapleader = " "
+
+vim.scriptencoding = "utf-8"
+opt.encoding = "utf-8"
+opt.fileencoding = "utf-8"
+
 opt.expandtab = true
 opt.shiftwidth = 4
 opt.tabstop = 4
 opt.softtabstop = 4
+opt.smarttab = true
+
+opt.scrolloff = 10
+opt.inccommand = "split"
+opt.ignorecase = true
+opt.breakindent = true
+opt.wrap = false
+opt.backspace = {"start", "eol", "indent"}
